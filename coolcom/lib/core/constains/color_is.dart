7fca01ef52913b0fd3a242cff975214b67ebf399
sifatullah-haque <sifatulla.haque@gmail.com>
2023-09-25 18:24:01 +0600

    new color added

--- coolcom/lib/core/constains/color_is.dart
+++ coolcom/lib/core/constains/color_is.dart
@@ -4,4 +4,5 @@ abstract class ColorIs {
   static const almostWhite = Color(0xffF6F6F6);
   static const kindaBlack = Color(0xff292526);
   static const itsGray = Color(0xffA4AAAD);
+  static const itsWhite = Color(0xffffffff);
 }
