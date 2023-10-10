6c892cd96a8d2590a9f73c12838bd407fa6e084c
sifatullah-haque <sifatulla.haque@gmail.com>
2023-10-10 23:21:26 +0600

    new bug fixed

---
+++ coolcom/lib/pages/error_solving.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Material App Bar'),
+          ),
+          body: Column(
+            children: [
+              Text("this is random text"),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("this is text"),
+              )
+            ],
+          )),
+    );
+  }
+}
