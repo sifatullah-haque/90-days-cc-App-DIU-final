5e59a1c0fd29b9e9064a4a0efa8ad9c975728b9b
sifatullah-haque <sifatulla.haque@gmail.com>
2023-10-18 22:15:21 +0600

    last color added

---
+++ coolcom/lib/core/constains/last_color_palet.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class ColorIs {
+  static const almostWhite = Color(0xffF6F6F6);
+  static const kindaBlack = Color(0xff292526);
+  static const itsGray = Color(0xffA4AAAD);
+  static const itsWhite = Color(0xffffffff);
+  static const itsBlack = Color(0xff000000);
+}
